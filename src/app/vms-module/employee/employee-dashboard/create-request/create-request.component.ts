2f041ab545f2b211c1ce9ce0bd5e1582460b4304
Shivam Sharma <shivam.sharma@team.telstra.com>
2019-03-06 16:05:17 +0530

    added approver and visitor type details

--- src/app/vms-module/employee/employee-dashboard/create-request/create-request.component.ts
+++ src/app/vms-module/employee/employee-dashboard/create-request/create-request.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  FormArray,
-  AbstractControl,
-  FormControl
-} from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { switchMap } from 'rxjs/operators';
 import { NgxSpinnerService } from "ngx-spinner";
+import { AuthService } from "src/app/auth/auth.service";
 import { VisitorService } from "src/app/vms-module/service/visitor.service";
 import { IMyOptions } from "mydatepicker";
 
@@ -17,40 +13,45 @@ import { IMyOptions } from "mydatepicker";
   styleUrls: ["./create-request.component.css"]
 })
 export class CreateRequestComponent implements OnInit {
+  visitorType: string;
   visitorForm: FormGroup;
-  visitorFormArray: FormArray;
   myDatePickerOptions: IMyOptions;
   maxLimit: number = 4;
 
   constructor(
+    private authService: AuthService,
     private formBuilder: FormBuilder,
-    private visitorService: VisitorService,
-    private spinner: NgxSpinnerService
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService,
+    private visitorService: VisitorService
   ) {}
 
   ngOnInit() {
-    this.configureDatePicker();
+    this.myDatePickerOptions = this.visitorService.configureDatePicker();
     this.createForm();
-    ///this.spinner.show();
   }
-
   createForm() {
-    this.visitorFormArray = this.formBuilder.array([this.createItem()]);
     this.visitorForm = this.formBuilder.group({
-      visitors: this.visitorFormArray
+      approverName: this.authService.user.empName,
+      approverEmployeeNo: this.authService.user.empNumber,
+      visitorType: null,
+      visitors: this.createVisitorArray()
     });
+    this.getVisitorType();
   }
 
-  createItem(): FormGroup {
+  createVisitorArray() {
+    // visitLocation: this.formBuilder.control("", [Validators.required]),
+    // visitorFromDate: [this.setDate(), Validators.required],
+    // visitorToDate: [this.setDate(), Validators.required],
+    // remarks: this.formBuilder.control("", Validators.maxLength(500))
+    return this.formBuilder.array([this.createVisitor()]);
+    
+
+  }
+
+  createVisitor(): FormGroup {
     return this.formBuilder.group({
-      requesterName: this.formBuilder.control({
-        value: "Shivam Sharma",
-        disabled: true
-      }),
-      requesterId: this.formBuilder.control({
-        value: "123133",
-        disabled: true
-      }),
       visitorName: this.formBuilder.control("", [
         Validators.required,
         Validators.pattern(/^[A-Za-z][A-Za-z ]{1,}[A-Za-z]$/)
@@ -67,64 +68,37 @@ export class CreateRequestComponent implements OnInit {
         Validators.pattern(/^[A-Za-z0-9_.]{3,}@[A-Za-z]+(\.[a-z]{2,}){1,2}$/)
       ]),
       visitorUIdType: this.formBuilder.control("", [Validators.required]),
-      visitorUId: this.formBuilder.control("", [Validators.required]),
-      visitLocation: this.formBuilder.control("", [Validators.required]),
-      visitorFromDate: [this.setDate(), Validators.required],
-      visitorToDate: [this.setDate(), Validators.required],
-      remarks: this.formBuilder.control("", Validators.maxLength(500))
+      visitorUId: this.formBuilder.control("", [Validators.required])
     });
   }
 
   addItem(): void {
-    this.visitorFormArray.push(this.createItem());
+    (<FormArray>this.visitorForm.get('visitors')).push(this.createVisitor());
+  }
+  
+  removeItem(i): void {
+    (<FormArray>this.visitorForm.get('visitors')).removeAt(i);
   }
 
-  removeItem(i): void {
-    this.visitorFormArray.removeAt(i);
+  resetForm() {
+    this.createForm();
   }
 
   submitForm() {
-    this.displayErrors(this.visitorForm);
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1000)
     this.visitorService.submitForm(this.visitorForm);
   }
-
-  displayErrors(form: AbstractControl) {
-    if (form instanceof FormControl) {
-      form.markAsTouched();
-    } else if (form instanceof FormGroup || form instanceof FormArray) {
-      Object.keys(form.controls).forEach(key => {
-        this.displayErrors(form.get(key));
-      });
-    }
+  
+  getVisitorType() {
+    this.route.paramMap.pipe(switchMap((params: ParamMap) => {
+      this.visitorForm.patchValue({visitorType: params.get('type')});
+      return params.get('type');
+    }))
+    .subscribe();
   }
 
-  configureDatePicker() {
-    let date = new Date();
-    this.myDatePickerOptions = {
-      dateFormat: "dd/mm/yyyy",
-      editableDateField: false,
-      disableUntil: new CustomDate(
-        date.getFullYear(),
-        date.getMonth() + 1,
-        date.getDate() - 1
-      ),
-      disableSince: new CustomDate(
-        date.getFullYear(),
-        date.getMonth() + 2,
-        date.getDate()
-      )
-    };
-  }
-
-  setDate() {
-    const date = new Date();
-    return {
-      year: date.getFullYear(),
-      month: date.getMonth() + 1,
-      day: date.getDate()
-    };
-  }
 }
-class CustomDate {
-  constructor(public year: number, public month: number, public day: number) {}
-}
+
--- src/app/vms-module/service/visitor.service.ts
+++ src/app/vms-module/service/visitor.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, AbstractControl, FormControl, FormArray } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CreateForm } from 'src/app/model/new-form';
+import { IMyOptions } from 'mydatepicker';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,53 @@ export class VisitorService {
   constructor(private restClient: HttpClient) { }
 
   submitForm(formGroup: FormGroup) {
-    if (this.validateForm(formGroup)) {
-      const createDate: CreateForm = Object.assign({}, formGroup.value, { submissionDate: new Date()}) 
-      console.log(createDate);
+    console.log(formGroup.value);
+    if (formGroup.valid) {
+      const createDate: CreateForm = Object.assign({}, formGroup.value, { submissionDate: new Date()});
+    } else {
+      this.validateForm(formGroup);
+    }
+  }
+  
+  validateForm(form: AbstractControl) {
+    if (form instanceof FormControl) {
+      form.markAsTouched();
+    } else if (form instanceof FormGroup || form instanceof FormArray) {
+      Object.keys(form.controls).forEach(key => {
+        this.validateForm(form.get(key));
+      });
     }
   }
 
-  validateForm(formGroup: FormGroup) {
-    formGroup.updateValueAndValidity();
-    return formGroup.valid;
+  configureDatePicker(): IMyOptions {
+    let date = new Date();
+    return {
+      dateFormat: "dd/mm/yyyy",
+      editableDateField: false,
+      disableUntil: new CustomDate(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        date.getDate() - 1
+      ),
+      disableSince: new CustomDate(
+        date.getFullYear(),
+        date.getMonth() + 2,
+        date.getDate()
+      )
+    };
   }
+
+  setDate() {
+    const date = new Date();
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,
+      day: date.getDate()
+    };
+  }
+
+}
+
+class CustomDate {
+  constructor(public year: number, public month: number, public day: number) {}
 }
